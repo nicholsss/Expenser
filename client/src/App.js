cd6e8c96598ec5d551e4e32a15c3480530f1e24c
nicholsss <mickyaw95@gmail.com>
2019-07-03 23:11:34 +0300

    hooks removed, login works

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,15 @@
 import React from "react";
 import LoginForm from "./components/LoginForm";
-import RegisterForm from "./components/RegisterForm";
-import {loginUser} from './reducers/loginReducer'
-
-import { connect } from "react-redux";
 const App = (props) => {
-  console.log('props',props.user)
-
-  
-  if(props.user ===null){
-    {console.log('props',props.user)}
-    return <LoginForm />
-  }
+  console.log('state', props)
   return (
     <div>    
-      <RegisterForm />
+      <LoginForm />
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return { user: state.user};
-};
-const mapDispatchToProps = {
-
-  loginUser,
- 
-};
 
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+
+export default App
